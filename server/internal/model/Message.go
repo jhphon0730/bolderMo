c32c7f781b0a449f3bedef383606c27c2a0a2088
jhphon0730 <eeung025@gmail.com>
2025-02-27 13:41:32 +0900

    feat : 클라이언트 모델과 동기화

--- server/internal/model/Message.go
+++ server/internal/model/Message.go
@@ -11,7 +11,14 @@ const (
 )
 
 type Message struct {
-	Type 		MessageType `json:"type"` // "message", "join", "leave"
-	Sender 	string `json:"sender"` // Client.ID
-	Data 		interface{} `json:"data"`
+	Type   MessageType `json:"type"`   // "message", "join", "leave"
+	Sender string      `json:"sender"` // Client.ID
+	Data   interface{} `json:"data"`
 }
+
+type MoveContent struct {
+	Direction string  `json:"direction"`
+	Dx        float64 `json:"dx"`
+	Dy        float64 `json:"dy"`
+}
+
