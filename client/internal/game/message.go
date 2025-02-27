77b6a71a417f99275c25ddc662097ce28f9db596
jeongho0730 <eeung025@gmail.com>
2025-02-27 13:41:53 +0900

    feat : handle character creation on client connection

--- client/internal/game/message.go
+++ client/internal/game/message.go
@@ -28,6 +28,19 @@ func (g *Game) handleServerMessage() {
 		select {
 		case msg := <-g.msgChan:
 			switch msg.Type {
+			case model.ClientConnected:
+				charImage, err := background.LoadCharImage()
+				if err != nil {
+					log.Fatal(err)
+				}
+				charImg := ebiten.NewImageFromImage(charImage)
+				char := &Character{
+					id:    msg.Sender,
+					x:     WINDOW_WIDTH / 2,
+					y:     WINDOW_HEIGHT / 2,
+					image: charImg,
+				}
+				g.characters = append(g.characters, char)
 			case model.ClientConnectedSuccess:
 				log.Printf("Client connected successfully: ID=%s", msg.Data.(string))
 				g.localID = msg.Data.(string)
